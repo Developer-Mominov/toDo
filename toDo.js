c26cf440075263ee9612b2c0e7cbd00eb99a72f5
Developer-Mominov <73039077+Developer-Mominov@users.noreply.github.com>
2021-05-05 16:41:29 +0500

    Update toDo.js

--- toDo.js
+++ toDo.js
@@ -1,183 +1,161 @@
-$(document).ready(function(){
-var sum=0;
 
-
-$(".xcv").click(function(){
-    $(this).parent().remove();
-})
-$("form").submit(function(event){
-    sum++;
-    event.preventDefault();
-    
-    var title = event.currentTarget['title'].value; // formani ichidagi title name li inputdan value olish
-    var t = new Date(event.currentTarget['time'].value).toLocaleDateString(); // formani ichidagi title name li inputdan value olish
-    var info = event.currentTarget['info'].value; // formani ichidagi title name li inputdan value olish
-    var color = event.currentTarget['color'].value; // formani ichidagi title name li inputdan value olish
-       
-    var html="<div class='sdf'> <div class='box b"+sum+"'>  <img src='./plan-idea.svg'> <div class='info'><h3>"+t+"<span>"+title+"</span>"+"</h3><p>"+ info+"</p></div></div> <button class='xcv'>X</button></div>";
-   
-    
- 
-    $('.list').append(html);
-
-    
-    var t="b"+sum;
-
-    $("."+t).css({
-        "background":color
-    });
- 
-    $(".xcv").click(function(){
-        $(this).parent().remove();
-    })
-    $("form").trigger('reset');
-
-    $("#hidden-form").css("display","none");
-    $(".main").css("display","block");
-    
-     
-
-})
-
-});
-var d=new Date();
- 
-
-function loading(){
-
-
+window.onload=function(){
     setTimeout(() => {
-        $(".load").css({
-            "opacity":"0",
-            "display":"none"
-        });
-
+        $('.loading').fadeOut(500);
     }, 1500);
-
-    setTimeout(() => {
-        $(".main").show(500);
-    }, 1750);
-
-    setTimeout(() => {
-        alert("this toDo app do not support firefox");
-    }, 2500);
-
-    
-    var sana=d.getDate();
-    var kunnav=d.getDay();
-    var oynav=d.getMonth();
-    var yilnav =d.getFullYear();
-  
-    var kunstring="";
-    var Oystring="";
-    
-
-    $('.k'+kunnav+"").css("border-top","2px solid indigo");
-
-    switch (kunnav) {
-        case 1:
-            kunstring="Monday"
-            break;
-            case 2:
-            kunstring="Tuesday"
-            break;
-            case 3:
-            kunstring="Wednesday"
-            break;
-            case 4:
-            kunstring="Thursday"
-            break;
-            case 5:
-            kunstring="Friday"
-            break;
-            case 6:
-            kunstring="Saturday"
-            break;
-            case 7:
-            kunstring="Sunday"
-            break;
-    
-        default:
-            break;
-    }
-
-    switch (oynav) {
-        case 0:
-            Oystring="January"
-            break;
-            case 1:
-            
-                Oystring="February"
-            break;
-
-            case 2:
-
-                Oystring="March"            
-            break;
-
-            case 3:
-            
-                Oystring="April"
-            break;
-
-            case 4:
-            
-                Oystring="May"
-            break;
-
-            case 5:
-            
-                Oystring="June"
-            break;
-
-
-
-            case 6:
-            
-                Oystring="July"
-            break;
-
-            case 7:
-            
-                Oystring="August"
-            break;
-
-            case 8:
-            
-                Oystring="September"
-            break;
-
-            case 9:
-            
-                Oystring="October"
-            break;
-
-            case 10:
-            
-                Oystring="November"
-            break;
-
-            case 11:
-            
-                Oystring="December"
-            break;
-            
-    
-        default:
-            break;
-    }
-
-
-
-     $("#sananav").text(sana);
-     $("#kunnav").text(kunstring);
-     $("#oynav").text(Oystring);
-     $("#yilnav").text(yilnav);
-
 }
 
-$("#add").click(function(){
-    $("#hidden-form").css("display","block");
-    $(".main").css("display","none");
+$(document).ready(function () {
+    var date = new Date();
+    $("#sana2").text(date.toLocaleDateString());    
+   
+
+    var sanoqb=0;
+
+    if(localStorage.sanoq == undefined){
+   
+        var arr=[];
+        localStorage.sanoq=JSON.stringify(arr);
+    }
+     
+    if(localStorage.sanoq != undefined){
+      arr=JSON.parse(localStorage.sanoq);
+      sanoqb=(JSON.parse(localStorage.sanoq).length)/2;
+      console.log("qiymat: "+sanoqb);
+      
+    }
+
+    if(sanoqb > 0){
+       
+
+        for (let i= 0;  i < sanoqb; i++) {
+            
+            if(JSON.parse(localStorage.sanoq)[i*2] != undefined){
+
+            $(".list").append('<div class="flexbox"> <div class="'+("rt rt"+i)+'">  <h4>'+( JSON.parse(localStorage.sanoq)[i*2] )+'</h4> <p>'+(JSON.parse(localStorage.sanoq)[i*2+1])+'</p> </div>  <button class="remove">X<button>  </div>');
+            console.log(i);
+ 
+        }
+
+        }
+
+        
+       
+    } 
+
     
-})
+
+
+
+    $("#add").click(function (){ 
+      
+        
+
+        $(".input").css({
+            "height":"100%",
+            "transition":"0.7s",
+            
+        })
+
+        
+    });
+
+    
+    
+  
+
+    
+
+
+     
+
+    $("#submit").click(function () { 
+        
+        
+        $(".input").css({
+            "height":"0",
+            "transition":"0.7s",
+        })
+                            
+
+
+
+        var title=$('#title').val();
+        var yozuv=$('#yozuv').val();
+        var sana=$('#sana').val();
+
+         arr.push(title+" "+sana);
+         arr.push(yozuv);
+
+         localStorage.sanoq=JSON.stringify(arr);
+
+
+        
+        
+         
+
+        
+
+       
+         $(".list").append('<div class="flexbox"> <div class="'+("rt rt"+sanoqb)+'">  <h4>'+( JSON.parse(localStorage.sanoq)[sanoqb*2] )+'</h4> <p>'+(JSON.parse(localStorage.sanoq)[sanoqb*2+1])+'</p> </div>  <button class="remove">X<button>  </div>');
+         sanoqb++;
+   
+        
+         $(".remove").click(function () { 
+     
+            var name=$(this).prev().attr("class");
+            var slname = name.slice(5,name.length);
+            slname=parseInt(slname)
+            delete arr[slname*2];
+            delete arr[slname*2+1];
+            localStorage.sanoq=JSON.stringify(arr);
+            $(this).parent().remove();
+         
+        });
+
+
+
+
+///////////////////////////////////////
+
+
+
+
+//////////////////////////////
+
+
+
+
+        var title=$('#title').val("");
+        var yozuv=$('#yozuv').val("");
+        var sana=$('#sana').val("");
+
+
+
+        
+        
+        
+
+        
+
+
+        
+    });
+
+ $(".remove").click(function () { 
+     
+    var name=$(this).prev().attr("class");
+    var slname = name.slice(5,name.length);
+    slname=parseInt(slname)
+    delete arr[slname*2];
+    delete arr[slname*2+1];
+    localStorage.sanoq=JSON.stringify(arr);
+    $(this).parent().remove();
+    
+    
+    
+     
+ });
+
+
+});
